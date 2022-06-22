60a819fa922ceeaa124add656c0a07915582c9d3
David-Z-1 <david.zheng20@imperial.ac.uk>
2022-06-22 19:55:14 +0100

    battery_position_adjusted_1

--- src/components/Signal.tsx
+++ src/components/Signal.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import BatteryCharging90Icon from '@mui/icons-material/BatteryCharging90';
 
 
 export default function Signal() {
   return (
+<Grid
+  container
+  direction="row"
+  justifyContent="flex-end"
+  alignItems="center"
+  marginTop={1.5}
+  marginLeft={3.5}
+>
     <IconButton>
-        <SignalCellularAltIcon sx={{color:'#fff',height:70, width:70, marginLeft:63.5, marginTop:2}}/>
+        <SignalCellularAltIcon sx={{color:'#fff',height:70, width:70}}/>
     </IconButton>
-  )
+
+    <IconButton sx={{variant:'contained', color:'#76ff03'}}>
+        <BatteryCharging90Icon sx={{height:70, width:70}}/>
+    </IconButton>
+</Grid>
+)
 }
 
